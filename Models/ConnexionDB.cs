ba68760f8b2de4f4b24ecf08a74285ec833624cd
nadiathyempabou3-cell <nadiathyempabou3@gmail.com>
2026-05-29 15:37:47 +0200

    Refactor database connection handling
    Refactored connection string retrieval and simplified connection methods.

--- Models/ConnexionDB.cs
+++ Models/ConnexionDB.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using MySql.Data.MySqlClient;
 
 namespace GestionBibliotheque.Models
@@ -7,57 +7,18 @@ namespace GestionBibliotheque.Models
     {
         private static string ObtenirChaineConnexion()
         {
-            string mysqlUrl = Environment.GetEnvironmentVariable("MYSQL_URL");
-            if (!string.IsNullOrEmpty(mysqlUrl))
-            {
-                try
-                {
-                    Uri uri = new Uri(mysqlUrl);
-                    string host = uri.Host;
-                    int port = uri.Port > 0 ? uri.Port : 3306;
-                    string database = uri.AbsolutePath.TrimStart('/');
-                    string userInfo = uri.UserInfo;
-                    string user = userInfo.Split(':')[0];
-                    string password = userInfo.Split(':').Length > 1 ? userInfo.Split(':')[1] : "";
-                    return $"Server={host};Port={port};Database={database};Uid={user};Pwd={password};SslMode=Preferred;AllowPublicKeyRetrieval=true;";
-                }
-                catch { }
-            }
-            string h = Environment.GetEnvironmentVariable("MYSQLHOST") ?? "127.0.0.1";
-            string p = Environment.GetEnvironmentVariable("MYSQLPORT") ?? "3307";
-            string db = Environment.GetEnvironmentVariable("MYSQLDATABASE") ?? "bibliotheque";
-            string u = Environment.GetEnvironmentVariable("MYSQLUSER") ?? "root";
-            string pwd = Environment.GetEnvironmentVariable("MYSQLPASSWORD") ?? "";
-            return $"Server={h};Port={p};Database={db};Uid={u};Pwd={pwd};SslMode=Preferred;AllowPublicKeyRetrieval=true;";
+            string host = Environment.GetEnvironmentVariable("MYSQLHOST") ?? "localhost";
+            string port = Environment.GetEnvironmentVariable("MYSQLPORT") ?? "3306";
+            string database = Environment.GetEnvironmentVariable("MYSQLDATABASE") ?? "bibliotheque";
+            string user = Environment.GetEnvironmentVariable("MYSQLUSER") ?? "root";
+            string password = Environment.GetEnvironmentVariable("MYSQLPASSWORD") ?? "";
+
+            return $"Server={host};Port={port};Database={database};Uid={user};Pwd={password};";
         }
 
         public static MySqlConnection ObtenirConnexion()
         {
-            try
-            {
-                MySqlConnection connexion = new MySqlConnection(ObtenirChaineConnexion());
-                connexion.Open();
-                return connexion;
-            }
-            catch (Exception ex)
-            {
-                throw new Exception("Erreur de connexion : " + ex.Message);
-            }
-        }
-
-        public static bool TesterConnexion()
-        {
-            try
-            {
-                using (MySqlConnection connexion = ObtenirConnexion())
-                {
-                    return connexion.State == System.Data.ConnectionState.Open;
-                }
-            }
-            catch
-            {
-                return false;
-            }
+            return new MySqlConnection(ObtenirChaineConnexion());
         }
     }
 }
